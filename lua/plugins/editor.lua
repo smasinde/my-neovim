7bf9f250e926e9248bc1fe6de5ff616c7755d277
Mabisi <smasinde@gmail.com>
2024-06-06 12:22:05 -0400

    remove unused plugins and added good ones

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/flash.nvim",
+    enabled = false,
+  },
+  {
+    "echasnovski/mini.animate",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.scroll = {
+        enable = false,
+      }
+    end,
+  },
+}
